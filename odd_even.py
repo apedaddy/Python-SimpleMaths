c164ca363d50ed67ae0275f3c311a4f44b0ba5af
apedaddy <ganesh.baral@hotmail.co.uk>
2019-06-24 08:21:23 +0100

    Find odd or even

---
+++ odd_even.py
@@ -0,0 +1,9 @@
+print ('Program to check odd or even: ')
+print ('______________________________')
+
+no = int(input("Enter your number: "))
+
+if (no%2 == 0):
+    print (f"Given number: {no} is even")
+else:
+    print (f"Given number: {no} is odd")
